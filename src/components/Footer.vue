7c5e489fe869860407dd71b3f4e295c509396913
th0ress0n <per@thoresson.com.au>
2019-08-22 21:51:24 +1000

    addig header and footer
    adding header and footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,21 +1,61 @@
 
 <template>
   <div id="footer" class="footer">
-    <div class="hr"></div>
-    <div class="container"></div>
+    <div class="top"></div>
+    <div class="middle">
+      <div id="centre-container" class="centre">
+        <div id="col-one" class="column">
+          <div id="col-one-title" class="column-title"></div>
+          <div id="col-one-body" class="column-body"></div>
+        </div>
+        <div id="col-two" class="column">
+          <div id="col-two-title" class="column-title"></div>
+          <div id="col-two-body" class="column-body"></div>
+        </div>
+        <div id="col-three" class="column">
+          <div id="col-three-title" class="column-title"></div>
+          <div id="col-three-body" class="column-body"></div>
+        </div>
+        <div id="col-four" class="column">
+          <div id="col-four-title" class="column-title"></div>
+          <div id="col-four-body" class="column-body"></div>
+        </div>
+      </div>
+    </div>
+    <div class="bottom"></div>
   </div>
 </template>
-<style scoped> 
-.footer {
-  position: fixed;
-  bottom: 0;
-  height: 100px;
-  line-height: 60px;
-  background-color: #015baf;
-}
 
-  .hr {
-    border-bottom: 1px solid #dbdbdb;
-    margin: 3em 0;
+<style scoped> 
+  .footer {
+    position: fixed;
+    bottom: 0;
+    height: 100px;
+    line-height: 60px;
+    background-color: #015baf;
   }
- </style>
+
+  .column-title{
+    font-weight: 700;
+    color: #ffffff
+  }
+
+  .column-body{
+    font-weight: 300;
+    color: #ededed
+  }
+
+  .top{
+    background-color: #09c!important;
+  }
+
+  .middle{
+    background-color: #1c2331!important;
+  }
+
+  .bottom{
+    background-color: #0f131b;
+    height: 20px;
+  }
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,25 @@
 <template functional>
-  <div id="header" slot="header">{{props.header}}</div>
+  <!-- <div id="header" slot="header">{{props.header}}</div> -->
+  <div id="header" class="header">
+    <div id="brand-logo" class="logo"><img src="../assets/ml-logo.png" alt="menu planer" class="logo-image"></div>
+  </div>
 </template>
 
-<style>
-#header{
+<style scoped>
+.header{
     margin: 0;
-    background-color: #38aa84;
+    background-color: #ffffff;
+}
+.logo{
+  display: inline-block;
+  margin-left: 30px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  width: 20%;
+  height:auto;
+}
+.logo-image{
+  width:100%;
+  height: auto;
 }
 </style>
